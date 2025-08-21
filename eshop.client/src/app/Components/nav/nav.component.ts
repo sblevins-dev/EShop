02bf99a57dd9b58148b6a9ca11dc8668d1cd4ddb
Stephen Blevins <61333980+sblevins-dev@users.noreply.github.com>
2025-08-21 15:06:19 -0400

    nav logic change to account for quantity

--- eshop.client/src/app/Components/nav/nav.component.ts
+++ eshop.client/src/app/Components/nav/nav.component.ts
@@ -9,4 +9,8 @@ import { CartService } from '../../Services/cart.service';
 })
 export class NavComponent {
   cartService = inject(CartService);
+
+  get cartItemCount(): number {
+  return this.cartService.getItems().reduce((total: number, item: any) => total + (item.quantity || 1), 0);
+}
 }
